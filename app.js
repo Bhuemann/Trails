496645dbedfabbc24cf3bbc2c405ec1656283e2f
Xu Han <han308@purdue.edu>
2016-04-23 21:53:49 -0400

    test node

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ app.listen(port);
 // for more info, see: http://expressjs.com
 var express = require('express');
 
+
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -132,3 +133,50 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
 });
 
+var MONGODB_URL="mongodb://han308:11OE4444@aws-us-east-1-portal.16.dblayer.com:10299/Trails"
+var mongoose = require('mongoose');
+var assert = require('assert');
+var fs = require('fs');
+
+var ca = [fs.readFileSync(__dirname + "/servercert.crt")];
+
+var options = {
+    mongos: {
+        ssl: true,
+        sslValidate: true,
+        sslCA:ca,
+    }
+}
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+mongoose.connection.on('open', function (err) {
+    assert.equal(null, err);
+    mongoose.connection.db.listCollections().toArray(function(err, collections) {
+        assert.equal(null, err);
+        collections.forEach(function(collection) {
+            console.log(collection);
+        });
+        mongoose.connection.db.close();
+        process.exit(0);
+    })
+});
+
+
+// Let's open that connection
+mongoose.connect(process.env.MONGODB_URL, options);
+/*
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+  // Create your schemas and models here.
+});
+
+mongoose.connect('mongodb://han308:11OE4444@aws-us-east-1-portal.16.dblayer.com:10299/Trails');
+*/
